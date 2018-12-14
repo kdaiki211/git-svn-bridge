8853cdd95f15b47114f5f0f0ee722e7d676a9ea9
Daiki Komatsuda <kdaiki211@gmail.com>
2018-12-14 09:31:42 +0900

    fix configure_svn script

--- scripts/internal/configure_svn.sh
+++ scripts/internal/configure_svn.sh
@@ -1,10 +1,20 @@
-#!/bin/bash -eu
+#!/bin/bash -eux
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 source $SCRIPT_DIR/conf_util.sh
 
 # SVN_CONFIG_CONF=~/.subversion/config
 SVN_SERVER_CONF=~/.subversion/servers
+
+if ! [ -e $SVN_SERVER_CONF ]; then
+    echo "file not found: $SVN_SERVER_CONF"
+    echo "generating $SVN_SERVER_CONF with svn command..."
+    svn help
+    if ! [ -e $SVN_SERVER_CONF ]; then
+        echo "failed to create $SVN_SERVER_CONF..."
+        exit 1
+    fi
+fi
 
 # set servers
 set_conf_value $SVN_SERVER_CONF 'store-plaintext-passwords' 'yes'
